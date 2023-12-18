25a37a27fe8741482d50cd2f76e8a38b053b8049
Anlprnc <anilpirincciii@gmail.com>
2023-12-18 23:03:19 +0300

    update-delete

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,9 +7,9 @@ const port = env.PORT;
 mongoose
   .connect(env.MONGO_CONNECTION_STRING)
   .then(() => {
-    console.log('Mongoose Connected!');
+    console.log('Mongoose connected');
     app.listen(port, () => {
-      console.log(`Server running on port -> ${port}`);
+      console.log('Server running on port: ' + port);
     });
   })
   .catch(console.error);
